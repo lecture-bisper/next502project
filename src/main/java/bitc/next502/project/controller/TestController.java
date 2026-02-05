0a44a6264f72d5d6fdd4bf16f1af3fafd5fce6df
DESKTOP-IVTH17U\it <bisper@outlook.kr>
2026-02-05 09:26:24 +0900

    프로젝트 세팅 완료.

---
+++ src/main/java/bitc/next502/project/controller/TestController.java
@@ -0,0 +1,28 @@
+package bitc.next502.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+  @GetMapping("/")
+  public String index() {
+    return "Hello World";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
